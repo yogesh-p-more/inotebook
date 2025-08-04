a3899edcfeedad1994ac0438877178ac03ecd37b
yogesh-p-more <yogesh03625@gmail.com>
2025-08-04 14:47:50 +0530

    video 49 completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const User = require('../models/User'); // Assuming you have a User model defined
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 router.post('/createuser', [
     body('name', 'Name must be at least 3 characters').isLength({ min: 3 }),
@@ -18,12 +20,23 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "User with this email already exists" });
         }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: secPass
         })
-        res.json(user);
+        const data = {
+            user: {
+                id: user.id
+            }
+        }   
+        const authtoken = jwt.sign(data , JWT_SECRET);
+        console.log(jwtData);
+        
+        res.json({authtoken});
+        // res.json(user);
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error");
