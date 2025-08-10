5b722f0cdd701ba46da42cceade95eb0d4110d75
yogesh-p-more <yogesh03625@gmail.com>
2025-08-10 16:10:52 +0530

    video 56 complte e

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import About from './component/About';
 
 function App() {
   return (
-   <>
-   <div>Yogesh</div>
-   </>
+    <>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
